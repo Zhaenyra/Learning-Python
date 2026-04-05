8b8190274cf445a315901370149374c89ebbae7e
Zhaenyra <humairaaa369@gmail.com>
2026-04-05 21:41:06 +0100

    Day5 - More on Python Lists

--- day5.py
+++ day5.py
@@ -1,15 +1,23 @@
-# Question 6 — ATM Machine
+# # Question 6 — ATM Machine
+# # Write a program that:
+
+# # Has a balance of ₦100,000
+# # Uses a while loop to keep asking the user for a PIN
+# # The correct PIN is 1234
+# # After 3 wrong attempts print Your card has been blocked! and stop
+# # Once logged in show a menu using while True:
+
+# # Option 1 — Check balance
+# # Option 2 — Withdraw money
+# # Option 3 — Exit
+
+
+# # For withdrawal check if user has sufficient funds
+# Question 5 — Guessing Game
 # Write a program that:
 
-# Has a balance of ₦100,000
-# Uses a while loop to keep asking the user for a PIN
-# The correct PIN is 1234
-# After 3 wrong attempts print Your card has been blocked! and stop
-# Once logged in show a menu using while True:
-
-# Option 1 — Check balance
-# Option 2 — Withdraw money
-# Option 3 — Exit
-
-
-# For withdrawal check if user has sufficient funds
+# Has a secret number of 42
+# Uses a while loop to keep asking the user to guess
+# Tells the user if their guess is too high, too low or correct
+# Counts how many attempts it took
+# Stops when the user guesses correctly
